486ff7f1cdd23b83a3e82de9cd90fcfcd66d76f8
Cucu Georgiana <georgianacucu24@gmail.com>
2023-12-23 19:46:35 +0200

    set2

--- src/exercise002.js
+++ src/exercise002.js
@@ -1,24 +1,32 @@
 export function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error('ingredients is required');
   // Your code here!
+  return sandwich.fillings;
 }
-
 export function isFromManchester(person) {
   if (person === undefined) throw new Error('person is required');
   // Your code here!
+  return person.city === 'Manchester';
 }
 
 export function getBusNumbers(people) {
   if (people === undefined) throw new Error('people is required');
   // Your code here!
+  const busesNeeded = Math.ceil(people / 40);
+  return busesNeeded;
 }
 
 export function countSheep(arr) {
   if (arr === undefined) throw new Error('arr is required');
   // Your code here!
+  const sheepCount = arr.filter((animal) => animal.toLowerCase() === 'sheep').length;
+  return sheepCount;
 }
 
 export function hasMPostCode(person) {
   if (person === undefined) throw new Error('person is required');
   // Your code here!
+  return (
+    person?.address?.city?.toLowerCase() === 'manchester' && person?.address?.postCode?.toUpperCase().startsWith('M')
+  );
 }
